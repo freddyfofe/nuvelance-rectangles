46a6a8cbdaa51fc4d972e7f4b4cc99b9f8d115a1
Freddy Forero <freddy.forero@gmail.com>
2022-07-09 18:00:46 -0500

    FF - Added edge test cases

--- src/test/java/com/faforerof/rectangles/services/implementation/RectangleOperationEdgeCasesTest.java
+++ src/test/java/com/faforerof/rectangles/services/implementation/RectangleOperationEdgeCasesTest.java
@@ -53,44 +53,4 @@ class RectangleOperationEdgeCasesTest {
         r2 = new Rectangle(0.0, 0.0, 8.0, 10.0);
         Assertions.assertFalse(testObject.intersects(r1, r2));
     }
-
-    @Test
-    void adjacentTrueWithProper() {
-        Rectangle r1 = new Rectangle(0.0, 0.0, 10.0, 10.0);
-        Rectangle r2 = new Rectangle(10.0, 0.0, 10.0, 5.0);
-        Assertions.assertEquals(Adjacency.PROPER, testObject.adjacent(r1, r2));
-
-        r1 = new Rectangle(0.0, 0.0, 10.0, 10.0);
-        r2 = new Rectangle(-5.0, 0.0, 10.0, 5.0);
-        Assertions.assertEquals(Adjacency.PROPER, testObject.adjacent(r1, r2));
-    }
-
-    @Test
-    void adjacentTrueWithSubLine() {
-        Rectangle r1 = new Rectangle(0.0, 0.0, 10.0, 10.0);
-        Rectangle r2 = new Rectangle(10.0, 0.0, 9.0, 5.0);
-        Assertions.assertEquals(Adjacency.SUB_LINE, testObject.adjacent(r1, r2));
-
-        r1 = new Rectangle(0.0, 0.0, 10.0, 10.0);
-        r2 = new Rectangle(-5.0, 0.0, 9.0, 5.0);
-        Assertions.assertEquals(Adjacency.SUB_LINE, testObject.adjacent(r1, r2));
-    }
-
-    @Test
-    void adjacentTrueWithPartial() {
-        Rectangle r1 = new Rectangle(0.0, 0.0, 10.0, 10.0);
-        Rectangle r2 = new Rectangle(10.0, 1.0, 100.0, 5.0);
-        Assertions.assertEquals(Adjacency.PARTIAL, testObject.adjacent(r1, r2));
-
-        r1 = new Rectangle(0.0, 0.0, 10.0, 10.0);
-        r2 = new Rectangle(10.0, 5.0, -10.0, 5.0);
-        Assertions.assertEquals(Adjacency.PARTIAL, testObject.adjacent(r1, r2));
-    }
-
-    @Test
-    void adjacentFalse() {
-        Rectangle r1 = new Rectangle(0.0, 0.0, 10.0, 10.0);
-        Rectangle r2 = new Rectangle(11.0, 10.0, 5.0, 5.0);
-        Assertions.assertEquals(Adjacency.NOT, testObject.adjacent(r1, r2));
-    }
 }
